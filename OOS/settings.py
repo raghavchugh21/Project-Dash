689cf37c3bd06ea6b95ff4bcd9a4bd44e65f1df6
Raghav Chugh <65908705+raghavchugh21@users.noreply.github.com>
2021-07-03 02:50:16 +0530

    Added support for Windows

--- OOS/settings.py
+++ OOS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['oos-env.eba-fmcyjsg4.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
