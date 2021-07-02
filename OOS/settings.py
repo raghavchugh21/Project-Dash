3bdb063087e2feaa3be73e2ce37ba525000065dd
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2021-07-03 01:19:32 +0530

    Update settings.py

--- OOS/settings.py
+++ OOS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['django-env.eba-vrmuvtai.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['oos-env.eba-fmcyjsg4.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
