e75839a5bba3e731f5220d724625fe2b0f153bb9
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2021-07-03 07:36:31 +0530

    Update settings.py

--- OOS/settings.py
+++ OOS/settings.py
@@ -153,7 +153,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-SITE_URL = "http://localhost:8000/"
+SITE_URL = "http://0.0.0.0/"
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
