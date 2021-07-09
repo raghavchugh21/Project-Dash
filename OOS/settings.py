2d4ab7c55350fd03e686618b7c90100845dcb218
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2021-07-09 18:26:35 +0530

    Fixing Errors

--- OOS/settings.py
+++ OOS/settings.py
@@ -138,10 +138,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'OOS/static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
