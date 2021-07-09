72b314d099e383466eb1b30c26f672e7f6240f3c
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2021-07-09 17:18:59 +0530

    Adding Allowed Host

--- OOS/settings.py
+++ OOS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', '18.117.73.160']
+ALLOWED_HOSTS = ['127.0.0.1', 'boiling-dawn-91107.herokuapp.com']
 
 
 # Application definition
