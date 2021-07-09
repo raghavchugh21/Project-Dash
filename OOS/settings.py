d961381c626961e37286f2708da905d59f4f3dd1
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2021-07-09 17:08:26 +0530

    Added more static files

--- OOS/settings.py
+++ OOS/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'OOS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': os.path.join(BASE_DIR, 'templates'),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -95,7 +95,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
