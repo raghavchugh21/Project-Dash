39f272a2d99ad1df30adaf0174c26db8a9c9eebf
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2021-07-09 17:03:48 +0530

    Added static/media files

--- OOS/settings.py
+++ OOS/settings.py
@@ -15,7 +15,7 @@ from django.contrib.messages import constants as messages
 from decouple import config
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -138,15 +138,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    'OOS/static'
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
