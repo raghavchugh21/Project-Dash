7d721e05dce7ff7ab5d316d3b3ef58fc17633ac4
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2021-07-12 07:28:49 +0530

    Add Allowed Host

--- OOS/settings.py
+++ OOS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://the-project-dash.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'the-project-dash.herokuapp.com']
 
 
 # Application definition
