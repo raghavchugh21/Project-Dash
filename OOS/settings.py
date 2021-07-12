b52f71e7266f92b2d6450a4765fa1dcca78d83a9
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2021-07-12 07:26:36 +0530

    Rename Project

--- OOS/settings.py
+++ OOS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'oos-project.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://the-project-dash.herokuapp.com/']
 
 
 # Application definition
